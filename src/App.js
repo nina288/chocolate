0614b40819c65a92855aa9c128fdac83a7ae8c33
nina288 <75220466+nina288@users.noreply.github.com>
2022-07-14 21:55:13 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import React from "react";
+import MyAuth from "./MyAuth";
+import SweetCard from "./SweetCard";
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+//import product from "./productData";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Navbar />
+        <MainContent />
+        <SweetCard />
+        <MyAuth />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/productData.js
@@ -0,0 +1,94 @@
+const product=[
+  {
+    id: 1,
+    image: <img src="/image/ld1.jpg" alt="sweets" />,
+    brand: "Lucky Days",
+    manufacter: "Кондитер Кубани ООО",
+    country: "Россия",
+    type_product: "Конфеты",
+    kindOfsweet: "Глазированные",
+    kind: "Молочный",
+    price: "81 р",
+    weight: "200 г",
+    selling: true
+  },
+  {
+    id: 2,
+    image: <img src="/image/ld2.jpg" alt="sweets" />,
+    brand: "Рот Фронт",
+    manufacter: "Объединенные кондитеры ООО",
+    country: "Россия",
+    type_product: "Конфеты",
+    kindOfsweet: "Батончики",
+    kind: "Молочный",
+    price: "140 р",
+    weight: "1 кг",
+    selling: true
+  },
+  {
+    id: 3,
+    image: <img src="/image/ld3.jpg" alt="sweets" />,
+    brand: "Конти",
+    manufacter: "ПО Конти АО",
+    country: "Россия",
+    type_product: "Конфеты",
+    kindOfsweet: "Желейные",
+    kind: "Фруктовые",
+    price: "60 р",
+    weight: "200 г",
+    selling: true
+  },
+  {
+    id: 4,
+    image: <img src="/image/ld4.jpg" alt="sweets" />,
+    brand: "Рот Фронт",
+    manufacter: "Объединенные кондитеры ООО",
+    country: "Россия",
+    type_product: "Конфеты",
+    kindOfsweet: "Шоколадные",
+    kind: "Кремовые",
+    price: "96 р",
+    weight: "200 г",
+    selling: true
+  },
+  {
+    id: 5,
+    image: <img src="/image/ld1.jpg" alt="sweets" />,
+    brand: "Золотой степ",
+    manufacter: "Славянка",
+    country: "Россия",
+    type_product: "Конфеты",
+    kindOfsweet: "Шоколадные",
+    kind: "Молочный",
+    price: "210 р",
+    weight: "300 г",
+    selling: false
+  },
+  {
+    id: 6,
+    image: <img src="/image/ld2.jpg" alt="sweets" />,
+    brand: "Красный Октябрь",
+    manufacter: "Кондитер Кубани ООО",
+    country: "Россия",
+    type_product: "Конфеты",
+    kindOfsweet: "Глазированные",
+    kind: "Суфлейная",
+    price: "120 р",
+    weight: "1 кг",
+    selling: true
+  },
+  {
+    id: 7,
+    image: <img src="/image/ld3.jpg" alt="sweets" />,
+    brand: "Красный Октябрь",
+    manufacter: "Красный Октябрь ПАО",
+    country: "Россия",
+    type_product: "Конфеты",
+    kindOfsweet: "Пралиновые",
+    kind: "Темный",
+    price: "136 р",
+    weight: "200 г",
+    selling: true
+  }
+]
+export default product;
